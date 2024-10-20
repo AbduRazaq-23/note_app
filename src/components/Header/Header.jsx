43ebe07f6eb60361a912d9c73622b871ae4a94a9
Abdurazaq333 <irazaqr986@gmail.com>
2024-10-20 16:38:37 +0500

    header color change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full  flex justify-center items-center h-20 px-5 fixed top-0 bg-gray-900">
+    <div className="w-full  flex justify-center items-center h-20 px-5 fixed top-0 bg-gray-950 ">
       <div className="w-full flex md:w-[30%]   px-2 p-1 rounded-md border border-slate-200">
-        <input className="bg-slate-900 w-full focus:outline-none" type="text" />
+        <input className="bg-gray-950 w-full focus:outline-none" type="text" />
         <button className="focus:outline-none">search</button>
       </div>
     </div>
